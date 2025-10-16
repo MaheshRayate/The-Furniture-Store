b10373a8b7f153a13812f3328e719d8a858151ac
MaheshRayate <maheshrayate16106227@gmail.com>
2025-10-16 15:39:56 +0530

    Updated Images Path

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -9,11 +9,12 @@ import {
 import { Card, CardContent } from '@/components/ui/card';
 
 import Image from 'next/image';
-import Hero1 from '/images/Hero1.jpg';
-import Hero2 from '/images/Hero2.jpg';
-import Hero3 from '/images/Hero3.jpg';
-import Hero4 from '/images/Hero4.jpg';
-import Hero5 from '/images/Hero5.jpg';
+
+import Hero1 from '@/public/Images/Hero1.jpg';
+import Hero2 from '@/public/Images/Hero2.jpg';
+import Hero3 from '@/public/Images/Hero3.jpg';
+import Hero4 from '@/public/Images/Hero4.jpg';
+import Hero5 from '@/public/Images/Hero5.jpg';
 
 const carouselImages = [Hero1, Hero2, Hero3, Hero4,Hero5];
 
